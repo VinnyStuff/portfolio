cd620481f3c130de943da30442d08f03dfb1012a
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-12-05 02:36:24 -0300

    funcionado o carousel , só falta com a imagem

--- script.js
+++ script.js
@@ -1,18 +1,49 @@
 window.onload = function() {
-    document.getElementsByClassName("carouselNavigationButton")[0].style.backgroundColor = "#0095F6";
+    const carouselNavigationButtons = document.getElementsByClassName("carouselNavigationButton");
+    carouselNavigationButtons[0].classList.add("carouselNavigationButtonActive");
 
     const carouselButtonInImage = document.getElementsByClassName('carouselButtonInImage');
-    for(var i = 0; i < carouselButtonInImage.length; i++) {
+    for(let i = 0; i < carouselButtonInImage.length; i++) {
         carouselButtonInImage[i].onclick = carouselNavigation;
     }
+   checker(carouselNavigationButtons);
 }
 
 function carouselNavigation(e){
-    console.log(e.target)
-    if(e.target.classList.contains("carouselNext")){
-        console.log("next");
+    let carouselNavigationButton = document.getElementsByClassName("carouselNavigationButton");
+
+    for(let i = 0; i < carouselNavigationButton.length; i++){
+        if(carouselNavigationButton[i].classList.contains("carouselNavigationButtonActive")){
+            if (e.target.classList.contains("carouselNext")){
+                if (i < carouselNavigationButton.length - 1){
+                    carouselNavigationButton[i].classList.remove("carouselNavigationButtonActive");
+                    carouselNavigationButton[i + 1].classList.add("carouselNavigationButtonActive");
+                }
+            }
+            else if(e.target.classList.contains("carouselPrev")){
+                if (i > 0){
+                    carouselNavigationButton[i].classList.remove("carouselNavigationButtonActive");
+                    carouselNavigationButton[i - 1].classList.add("carouselNavigationButtonActive");
+                }
+            }
+            checker(carouselNavigationButton);
+            break;
+        }
     }
-    else if (e.target.classList.contains("carouselPrev")){
-        console.log("prev");
+}
+
+function checker(carouselNavigationButtons){
+    if (carouselNavigationButtons[0].classList.contains("carouselNavigationButtonActive")){
+        document.getElementsByClassName("carouselPrev")[0].style.display = "none";
     }
+    else{
+        document.getElementsByClassName("carouselPrev")[0].style.display = "block";
+    }
+    if (carouselNavigationButtons[carouselNavigationButtons.length - 1].classList.contains("carouselNavigationButtonActive")){
+        document.getElementsByClassName("carouselNext")[0].style.display = "none";
+    }
+    else {
+        document.getElementsByClassName("carouselNext")[0].style.display = "block";
+    }
+
 }
