0c962b78699997f587d51a1c586ddd71f43f8357
VinnyStuffs <70545559+VinnyStuffs@users.noreply.github.com>
2021-12-11 01:16:12 -0300

    iniciando redes sociais

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 window.onload = function() {
-    document.getElementsByClassName("images")[0].onclick = function (){
-        console.log("Open the project");
-    };
+    projectsOpen();
+}
+
+function projectsOpen(){
+    document.getElementById("asteroidSpace").onclick = function(){
+        window.open("https://play.google.com/store/apps/details?id=com.VinnyStuff.AsteroidSpaceGooglePlay&utm_source=vinnystuff.com&utm_campaign=site&pcampaignid=pcampaignidMKT-Other-global-all-co-prtnr-py-PartBadge-Mar2515-1");
+    }
+    document.getElementById("bmi").onclick = function(){
+        window.open("https://vinnystuff.github.io/BMI-BMR-Tracker/");
+    }
+    document.getElementById("athleticsCalculator").onclick = function(){
+        window.open("https://vinnystuff.github.io/athletics-calculator/");
+    }
 }
